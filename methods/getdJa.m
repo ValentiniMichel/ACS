cb87ef3a00e34875c381aec234bd79f78114d104
ValentiniMichel <65706284+ValentiniMichel@users.noreply.github.com>
2023-03-01 12:52:01 +0100

    Add files via upload

--- methods/getdJa.m
+++ methods/getdJa.m
@@ -9,19 +9,9 @@ dq1 = dq(1);
 dq2 = dq(2);
 dq3 = dq(3);
 
-%dJa = [
-%[-a3*(cos(q1+q3)*(diff(q1)+diff(q3))) - a1*(cos(q1)),   0, -a3*(cos(q1+q3)*(diff(q1)+diff(q3)))]
-%[ a3*(-sin(q1+q3)*(diff(q1)+diff(q3))) + a1*(-sin(q1)), 0, a3*(-sin(q1+q3)*(diff(q1)+diff(q3)))]
-%[                                                   0,  1,                                    0]
-%[                                                   1,  0,                                    1]
-%[                                                   0,  0,                                    0]
-%[                                                   0,  0,                                    0]
-%];
-
-
 dJa = [
-[-a3*(cos(q1+q3)*(dq1+dq3)) - a1*(cos(q1)),    0,  -a3*(cos(q1+q3)*(dq1+dq3))]
-[a3*(-sin(q1+q3)*(dq1+dq3)) + a1*(-sin(q1)),   0,  a3*(-sin(q1+q3)*(dq1+dq3))]
+[-a3*(cos(q1+q3)*(dq1+dq3)) - a1*(cos(q1)*dq1),    0,  -a3*(cos(q1+q3)*(dq1+dq3))]
+[a3*(-sin(q1+q3)*(dq1+dq3)) + a1*(-sin(q1)*dq1),   0,  a3*(-sin(q1+q3)*(dq1+dq3))]
 [                                           0,  0,                          0]
 [                                           0,  0,                          0]
 [                                           0,  0,                          0]
